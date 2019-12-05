3b86b0a85eed030a85a04ab3b4a23090100715fa
yuyonshen <2280629901@qq.com>
2019-12-05 22:54:41 +0800

    指针

---
+++ 19/19/19.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){//Ö¸ÕëÕûÊı
+	int n = 10;
+	char *pc = (char*)&n;
+	int *pi = &n;
+	printf("%p\n", &n);
+	printf("%p\n", pc);
+	printf("%p\n", pc+1);
+	printf("%p\n", pi);
+	printf("%p\n", pi+1);
+	system("pause");
+	return 0;
+}
