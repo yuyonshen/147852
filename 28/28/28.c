fc1e766625e355e86d39ed4bd3f999636b9d03d4
yuyonshen <2280629901@qq.com>
2019-12-08 19:17:37 +0800

    第五章练习

---
+++ 28/28/28.c
@@ -0,0 +1,12 @@
+/*#include<stdio.h>
+int main(){//定义一个变量进行自增自减
+	int a = 6;
+	int x1, x2, x3, x4;
+			x1 = ++a;
+		x2 = a++;
+		x3 = --a;
+		x4 = a--;
+	printf("%d\n%d\n%d\n%d\n", x1, x2, x3, x4);
+	system("pause");
+	return 0;
+}*/
