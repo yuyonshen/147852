c637fbc415ac970150933e0a3a1397d32e042170
yuyonshen <2280629901@qq.com>
2019-12-21 11:29:00 +0800

    java3

---
+++ Test2.java
@@ -0,0 +1,8 @@
+public class Test2{
+    public static void main(String[] args) {
+        int a = 130; 
+         double b = 3.5;
+    System.out.println("a:"+a);
+    System.out.println("b:"+b);
+        }
+}
