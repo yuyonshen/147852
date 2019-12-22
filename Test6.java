695e4d5b9aea7333c497da2b35f610e1aa25c7b6
yuyonshen <2280629901@qq.com>
2019-12-22 20:27:20 +0800

    java1

---
+++ Test6.java
@@ -0,0 +1,12 @@
+
+public class Test6{//交换两个变量的值
+    public static void main(String[] args) {
+     int a=10;
+     int b=20;
+     int c=a;
+     a=b;
+     b=c;
+     System.out.println("a= "+ a + "b= "+ b );
+    }
+
+}
