8daf9790ec549cfcd80c036ef0b188846c056f5b
yuyonshen <2280629901@qq.com>
2019-12-24 19:21:03 +0800

    java 初4

---
+++ Test25.java
@@ -0,0 +1,14 @@
+
+
+public class Test25{
+    public static void main(String[] args) {
+        int a=6543;
+        int count=4;
+        int num;
+        for(int i=0;i<count;i++){
+        num=a%10;
+        a=a/10;
+            System.out.println(num);
+    }
+    }
+}
