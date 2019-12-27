beab980d6869fb6e51dbe1d5ff9a67dbb3d7389c
yuyonshen <2280629901@qq.com>
2019-12-27 13:02:40 +0800

    Java 数组3

---
+++ Test38.java
@@ -0,0 +1,11 @@
+public class Test38{
+    public static void main(String[] args) {
+        int[] arr={1,5,3};
+        printArray(arr);
+    }
+    public static void printArray(int[] a){
+        for(int x:a){
+            System.out.println(x);
+        }
+    }
+}
