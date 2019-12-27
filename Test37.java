7907b4ed6ad027006c7a49e5c6bb48deaa19ec98
yuyonshen <2280629901@qq.com>
2019-12-27 13:02:13 +0800

    Java 数组2

---
+++ Test37.java
@@ -0,0 +1,16 @@
+/*public class Test37{
+    public static void main(String[] args) {
+        int[] arr={1,2,3};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}*/
+public class Test37{
+    public static void main(String[] args) {
+        int[] arr={1,2,3};
+        for(int i:arr){
+            System.out.println(i);
+        }
+    }
+}
