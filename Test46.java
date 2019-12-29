539c0bb89a6817785569e6d2bab4d6ac6feffb0d
yuyonshen <2280629901@qq.com>
2019-12-29 20:28:52 +0800

    作业2 1

---
+++ Test46.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+//编写代码: 创建一个 int 类型的数组, 元素个数为 100, 并把每个元素依次设置为 1 - 100
+public class Test46{
+    public static void main(String[] args) {
+        int[] arr=new int[100];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=i;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
