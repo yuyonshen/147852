ea9b1b61d84e769a811d2893a015452ef5b585c6
yuyonshen <2280629901@qq.com>
2020-01-01 18:58:54 +0800

    练习1

---
+++ Test67.java
@@ -0,0 +1,12 @@
+
+
+public class Test67{
+    public static void main(String[] args) {
+        short x=~123;
+        System.out.println("12与8的结果:"+(12&8));
+        System.out.println("4与8的结果:"+(4|8));
+        System.out.println("31与22的结果:"+(31^22));
+        System.out.println((2>3&4!=7));
+        System.out.println((2>3|4!=7));
+    }
+}
