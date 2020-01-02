cf9379db883802c7ee1d80d2a689bf078488feb5
yuyonshen <2280629901@qq.com>
2020-01-02 23:24:28 +0800

    类1

---
+++ Test70.java
@@ -0,0 +1,13 @@
+package Person;
+
+public class Test1 {
+    public String name;
+    public int age;
+}
+class Test{
+    public static void main(String[] args) {
+        Test1 test1=new Test1();
+        System.out.println(test1.name);
+        System.out.println(test1.age);
+    }
+}
