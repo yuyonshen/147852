1f5d861b0170b9f8d813a6093828fa140e1988fa
yuyonshen <2280629901@qq.com>
2020-01-09 15:33:45 +0800

    寒假2

---
+++ Test92.java
@@ -0,0 +1,11 @@
+public class Test92 {
+    public static void main(String args[]) {
+        int i = 7;
+    do {
+    System.out.println(--i);
+    --i;
+    }
+     while (i != 0);
+    
+    }
+    }
