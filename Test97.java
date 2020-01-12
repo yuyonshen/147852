9a8117e81e3c69466ab32b7c8aaabb35c0b831ab
yuyonshen <2280629901@qq.com>
2020-01-12 22:36:55 +0800

    shujia

---
+++ Test97.java
@@ -0,0 +1,11 @@
+public class Test97{
+    public static void main(String[] args) {
+      // int[] arr={1,2,3,4,5,6};
+      // System.out.println(arr.length);
+    //String arr="hello word";
+  //  char[] arr1=arr.toCharArray();
+    //System.out.println(arr1.length);
+        char[] arr="hello";
+        System.out.println(arr.length);
+}
+}
