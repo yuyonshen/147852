fb49f5faa5fbaee3749deac3c4d35336f774df4f
yuyonshen <2280629901@qq.com>
2020-01-13 19:38:30 +0800

    暑假5

---
+++ Test100.java
@@ -0,0 +1,11 @@
+
+public class Test100 {
+private float f = 1.0f;
+int m = 12;
+static int n = 1;
+public static void main (String args[]) {
+Test100 t = new Test100();
+System.out.println(t.f);
+
+}
+}
