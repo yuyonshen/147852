58773c59d472608744a614b655783a868841cf03
yuyonshen <2280629901@qq.com>
2020-01-13 19:39:20 +0800

    暑假6

---
+++ Test101.java
@@ -0,0 +1,12 @@
+public class Test101{
+    public static void main(String[] args) {
+        String str="hello word";
+        int size=str.length();
+        char[] arr=str.toCharArray();
+        for(int i=0;i<arr.length;i++){
+        System.out.println(str.charAt(i));
+        }
+      //  System.out.println(size);
+       // System.out.println(arr);
+    }
+}
