4b21b468d7ceac2a0648c425d093e4327dd1fc65
yuyonshen <2280629901@qq.com>
2020-01-15 18:23:44 +0800

    15_4

---
+++ java1_15_5.java
@@ -0,0 +1,15 @@
+public class java1_15_5 {
+    public static void main(String[] args) {
+//        String arr="我是一个字符串";
+//        char[] ch=arr.toCharArray();
+//        for(int i=0;i<ch.length;i++){
+//            System.out.println("数组第"+i+"个元素  ："+ch[i]);
+//        }
+//        char[] charArray={'t','i','m','e'};
+//        String a=new String(charArray);
+//        System.out.println(a);
+        String str="123445567asdfghkuyjt";
+        boolean a=str.contains("123");
+        System.out.println(a);
+    }
+}
