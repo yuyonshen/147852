68093193f9f1bdc2d964cc72085d966de4e68487
yuyonshen <2280629901@qq.com>
2020-02-07 15:14:07 +0800

    1_7_2

---
+++ java1_7_2.java
@@ -0,0 +1,10 @@
+//字符串大小转换；
+public class java1_7_2 {
+    public static void main(String[] args) {
+        String str="abc DEF";
+        //转化为小写字符串
+        System.out.println(str.toLowerCase());
+        //转化为大写字符串
+        System.out.println(str.toUpperCase());
+    }
+}
