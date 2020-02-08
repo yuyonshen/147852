691aa5544a185b0d2783a665bfabed67d7c86fe2
yuyonshen <2280629901@qq.com>
2020-02-08 17:13:10 +0800

    j 1_8_4

---
+++ StringBufferTest.java
@@ -0,0 +1,32 @@
+import com.sun.org.apache.xpath.internal.operations.String;
+
+public class StringBufferTest {
+//    public static void main(String[] args) {
+//        //创建字符串序列；
+//        StringBuffer sbf=new StringBuffer("ABCDEFG");
+//        //输出原值；
+//        System.out.println("sbf的原始值为："+sbf);
+//        //获取字符串的长度；
+//        int length=sbf.length();
+//        //输出长度；
+//        System.out.println("sbf的长度为："+length);
+//        //获取引索为5的字符；
+//        char chr=sbf.charAt(5);
+//        //输出指定引索的字符；
+//        System.out.println("引索为5的字符为："+chr);
+//        //获取def字符串所在引索位置；
+//        int index=sbf.indexOf("DEF");
+//        //输出子字符串引索；
+//        System.out.println("DEF字符串的引索位置为："+index);
+//        //截取子字符串的字符；
+//        String substr=sbf.substring(0,2);
+//        //输出截取结果；
+//        System.out.println("引索0开始值引索2之间的字符串："+substr);
+//        //替换引索之间的字符；
+//        StringBuffer tmp=sbf.replace(2,5,"1234");
+//        System.out.println("替换后的字符串："+tmp);
+//    }
+
+
+
+}
