1b7bb3a1b19ff9c4549f05f0f4b64ba06633feb7
yuyonshen <2280629901@qq.com>
2020-02-16 14:26:58 +0800

    j 2_16_1

--- Demo.java
+++ Demo.java
@@ -1,17 +1,22 @@
 public class Demo {
-    public static void main(String args[]){
-        int num = 10;
-        System.out.println(test(num));
-    } public static int test(int b){
-        try {
-            b += 10;
-            return b;
-        }catch(RuntimeException e){
-        }catch(Exception e2){
-        } finally{
-            b += 10;
-            return b;
-        }
-
+    public static void main(String[] args) {
+        //将字符串转换为int类型
+        int num=Integer.parseInt("456");
+        //通过构造函数创建一个Integer对象
+        Integer iNum=new Integer("456");
+        System.out.println("int数据以与Integer对象的比较"+iNum.equals(num));
+    //获取数字的二进制表示
+        String str2=Integer.toBinaryString(num);
+        //获取数字十六进制表示
+        String str3=Integer.toHexString(num);
+        //获取数字八进制表示
+        String str4=Integer.toOctalString(num);
+        //获取数字15进制表示
+        String str5=Integer.toString(num,15);
+        System.out.println("456的二进制表示: "+str2);
+        System.out.println("456的十六进制表示: "+str3);
+        System.out.println("456的八进制表示: "+str4);
+        System.out.println("456的十五进制表示: "+str5);
+       
     }
 }
