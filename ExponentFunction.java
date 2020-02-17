055a36871e2c4f35f68d11b553668e679a9b79f8
yuyonshen <2280629901@qq.com>
2020-02-17 16:35:11 +0800

    j 2_17_4

---
+++ ExponentFunction.java
@@ -0,0 +1,10 @@
+public class ExponentFunction {
+    public static void main(String[] args) {
+        System.out.println("e的平方"+Math.exp(2));
+        System.out.println("以e为底数，2为对数："+Math.log(2));
+        System.out.println("以10为底数2为对数："+Math.log10(2));
+        System.out.println("4的算术平方根："+Math.sqrt(4));
+        System.out.println("8的立方根："+Math.cbrt(8));
+        System.out.println("2的平方："+Math.pow(2,2));
+    }
+}
