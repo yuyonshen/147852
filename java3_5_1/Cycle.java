006e8abb35e3f7c6dcb27d3b3baba63e34ec38cf
yuyonshen <2280629901@qq.com>
2020-03-05 17:15:20 +0800

    j 3_5_4

---
+++ java3_5_1/Cycle.java
@@ -0,0 +1,8 @@
+package java3_5_1;
+
+public class Cycle extends Shape {
+    @Override
+    public void draw(){
+        System.out.println("⚪");
+    }
+}
---
+++ java3_5_1/Shape.java
@@ -0,0 +1,8 @@
+package java3_5_1;
+
+public class Shape {
+    public void draw(){
+        //啥都不干
+    }
+}
+
