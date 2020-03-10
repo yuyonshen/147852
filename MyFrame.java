fa99973f96f0e0bd83e971f386e39ba2c39605dc
yuyonshen <2280629901@qq.com>
2020-03-10 16:45:18 +0800

    j 3_10_5

---
+++ MyFrame.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+ class MyJDialog extends JDialog {
+    public MyJDialog(MyFrame frame) {
+        super(frame, "第一个对话框", true);
+        Container container=getContentPane();
+        container.add(new JLabel("这是一个对话框"));
+        setBounds(120,120,100,100);
+    }
+
+
+}
+public class MyFrame extends JFrame{
+    public MyFrame(){
+        Container container=getContentPane();
+        container.setLayout(null);
+        JButton bl=new JButton("弹出对话框");
+        bl.setBounds(10,10,100,20);
+        bl.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MyJDialog dialog=new MyJDialog(MyFrame.this);
+                dialog.setVisible(true);
+            }
+        });
+        container.add(bl);
+        container.setBackground(Color.black);
+        setSize(200,300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MyFrame();
+    }
+
+}
