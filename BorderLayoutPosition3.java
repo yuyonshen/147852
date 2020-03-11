dca6dd54ce604a8151bfea91c7a64688d99fa3e9
yuyonshen <2280629901@qq.com>
2020-03-11 14:45:51 +0800

    j 3_11_1

--- BorderLayoutPosition3.java
+++ BorderLayoutPosition3.java
@@ -1,21 +1,28 @@
+import jdk.internal.org.objectweb.asm.tree.MultiANewArrayInsnNode;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class BorderLayoutPosition3 extends JFrame {
     public BorderLayoutPosition3(){
-        setTitle("这个窗口使用边界管理器");
-        Container c=getContentPane();
+        setTitle("这个窗体使用边界布局管理器");
+        Container container = getContentPane();
         setLayout(new BorderLayout());
-        JButton a=new JButton("中");
-        JButton b=new JButton("北");
-        JButton f=new JButton("南");
-        JButton d=new JButton("西");
-        JButton e=new JButton("东");
-        c.add(a,BorderLayout.CENTER);
-        c.add(b,BorderLayout.NORTH);
-        c.add(f,BorderLayout.SOUTH);
-        c.add(d,BorderLayout.WEST);
-        c.add(e,BorderLayout.EAST);
+        JButton jb1=new JButton("中");
+        jb1.setBackground(Color.white);
+        JButton jb2=new JButton("北");
+        jb2.setBackground(Color.BLACK);
+        JButton jb3=new JButton("南");
+        jb3.setBackground(Color.BLACK);
+        JButton jb4=new JButton("西");
+        jb4.setBackground(Color.BLACK);
+        JButton jb5=new JButton("东");
+        jb5.setBackground(Color.BLACK);
+        container.add(jb1,BorderLayout.CENTER);
+        container.add(jb2,BorderLayout.NORTH);
+        container.add(jb3,BorderLayout.SOUTH);
+        container.add(jb4,BorderLayout.WEST);
+        container.add(jb5,BorderLayout.EAST);
         setSize(350,200);
         setVisible(true);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
