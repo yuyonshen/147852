0459fe6bc3062600a79e5e8ba240cecdde3cf9dc
yuyonshen <2280629901@qq.com>
2020-03-17 16:42:13 +0800

    j 3_17_1

---
+++ Solution11.java
@@ -0,0 +1,33 @@
+public class Solution11 {
+    public int countCharacters(String[] words, String chars) {
+        int[] chars_count = count(chars);
+        int res=0;
+        for (String word : words) {
+            int[] word_count = count(word); // 统计单词的字母出现次数
+            if (contains(chars_count, word_count)) {
+                res += word.length();
+            }
+        }
+        return res;
+    }
+
+
+    // 检查字母表的字母出现次数是否覆盖单词的字母出现次数
+    boolean contains(int[] chars_count, int[] word_count){
+        for (int i = 0; i < 26; i++) {
+            if (chars_count[i] < word_count[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    //字母出现在次数
+    int[] count(String word){
+        int[] counter=new int[26];
+        for(int i=0;i<word.length();i++){
+            char c=word.charAt(i);
+            counter[c-'a']++;
+        }
+        return counter;
+    }
+}
