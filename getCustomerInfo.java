d17155df7500c947e63cacd2b25816e1bd51756f
yuyonshen <2280629901@qq.com>
2020-03-20 12:45:55 +0800

    j 3_20_2

---
+++ getCustomerInfo.java
@@ -0,0 +1,17 @@
+public class getCustomerInfo {
+    public void getCustomerInfo() {
+        try {
+
+            // do something that may cause an Exception
+
+
+        } catch (
+                java.lang.Exception ex) {
+
+            System.out.print("Exception!");
+
+        }
+    }
+
+}
+
