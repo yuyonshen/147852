33c735747843b942bf7efea4554c19242ce5748f
yuyonshen <2280629901@qq.com>
2020-03-22 21:13:02 +0800

    j 3_22_1

--- Alpha.java
+++ Alpha.java
@@ -1,23 +1,12 @@
 class Base {
-
-    Base() {
-
+        Base() {
         System.out.print("Base");
-
-    }
-
-}
-
-public class Alpha extends Base {
-
+        }
+        }
+        public class Alpha extends Base {
     public static void main( String[] args ) {
-
         new Alpha();
-
-        //调用父类无参的构造方法
-
+//调用父类无参的构造方法
         new Base();
-
     }
-
 }
