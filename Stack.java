63a0f8ef3c5e0fb72b406896f6098a46d3701c56
yuyonshen <2280629901@qq.com>
2020-03-27 21:33:17 +0800

    栈

---
+++ Stack.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Stack {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        //int[] arr=new int[100];
+        scanner.nextInt();
+        System.out.println(push());
+
+    }
+
+    private static int push() {
+        return 0;
+    }
+}
